6d0765bb918347546eeb7fe9e314d99c0cfda5cc
Bagesh jha <bageshjha7@gmail.com>
2024-11-02 11:09:10 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name  = "medibot",
+    version = "0.0.0",
+    author = "Bagesh Jha",
+    author_email = 'bageshjha7@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
